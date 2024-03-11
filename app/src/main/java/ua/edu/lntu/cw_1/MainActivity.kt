d67e899975e57208b14a9e55ff305d05192a7bfc
mayday <antoshakarpyuk@gmail.com>
2024-03-11 14:44:25 +0200

    First contact

--- app/src/main/java/ua/edu/lntu/cw_1/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_1/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -19,6 +22,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ua.edu.lntu.cw_1.ui.theme.IPZ_CW_1_Karpyuk_AntonTheme
+import java.util.concurrent.ConcurrentLinkedDeque
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,22 +44,44 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Card(name: String, ) {
     Column {
-        Image(
-            painter = painterResource(id = R.drawable.flower),
-            contentDescription = "Settings Icon",
-            modifier = Modifier
-                .width(224.dp)
-        )
-        Text(
-            text = "Karpyuk Anton",
-            modifier = Modifier,
-            fontSize = 36.sp
-        )
-        Text(
-            text = "Software engineer student",
-            modifier = Modifier,
-            fontSize = 24.sp
-        )
+        Column(modifier = Modifier) {
+            Image(
+                painter = painterResource(id = R.drawable.flower),
+                contentDescription = "Settings Icon",
+                modifier = Modifier
+                    .width(224.dp)
+                    .padding(5.dp)
+            )
+            Text(
+                text = "Karpyuk Anton",
+                modifier = Modifier
+                    .padding(5.dp),
+                fontSize = 36.sp
+            )
+            Text(
+                text = "Software engineer student",
+                modifier = Modifier
+                    .padding(5.dp),
+                fontSize = 24.sp
+            )
+        }
+        Column {
+            Row {
+                Image(
+                    painter = painterResource(id = R.drawable.flower),
+                    contentDescription = "Settings Icon",
+                    modifier = Modifier
+                        .width(50.dp)
+                        .padding(5.dp)
+                )
+                Text(
+                    text = "+00 (00) 000 000",
+                    modifier = Modifier
+                        .padding(5.dp),
+                    fontSize = 24.sp
+                )
+            }
+        }
     }
 }
 
